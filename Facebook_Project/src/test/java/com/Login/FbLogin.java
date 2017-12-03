f92f02e92faec580351c4b2c06d3734a467305e8
Diya Bala <Diya Bala@DESKTOP-4G3ERNM>
2017-12-03 10:06:28 -0600

    new project

---
+++ Facebook_Project/src/test/java/com/Login/FbLogin.java
@@ -0,0 +1,13 @@
+package com.Login;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FbLogin {
+
+	public static void main(String[] args) {
+		
+ ChromeDriver driver=new ChromeDriver();
+ driver.get("welcome to maven project");
+	}
+
+}
